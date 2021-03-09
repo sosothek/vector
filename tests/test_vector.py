5fa3e26797d4014545af3c0df07e7626aa902339
pcotte <pcotte@advestis.com>
2021-03-09 19:45:03 +0100

    new: usr: implementing argument

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,11 +1,31 @@
 from vector import Vector
+from math import pi, sqrt
 
 v = Vector(3, 4)
 v2 = Vector(1, 2)
+v3 = Vector(1, 0)
+v4 = Vector(0, 1)
+v5 = Vector(-1, 0)
+v6 = Vector(1, 1)
+v7 = Vector(1, -1)
+v8 = Vector(-1, 1)
+v9 = Vector(-1, -1)
+v10 = Vector(0, 0)
 
 
 def test_norm():
     assert v.norm == 5
+
+
+def test_argument():
+    assert v3.arg == 0
+    assert v4.arg == pi / 2.
+    assert v5.arg == pi
+    assert round(v6.arg, 6) == round(pi / 4, 6)
+    assert round(v7.arg, 6) == -round(pi / 4, 6)
+    assert round(v8.arg, 6) == round(3 * pi / 4, 6)
+    assert round(v9.arg, 6) == -round(3 * pi / 4, 6)
+    assert v10 is None
 
 
 def test_access():
--- vector/vector.py
+++ vector/vector.py
@@ -1,3 +1,19 @@
+import math
+
+
 class Vector:
     def __init__(self, x, y):
-        pass
+        self.x = x
+        self.y = y
+        self.norm = math.sqrt(x*x + y*y)
+        if x != 0 and y >= 0:
+            self.arg = math.acos(x / self.norm)
+        elif y != 0 and x > 0:
+            self.arg = math.asin(y / self.norm)
+        elif x < 0 and y < 0:
+            self.arg = -math.acos(x / self.norm)
+        else:
+            self.arg = None
+
+    def scalar(self, v):
+        return self.x * v.x + self.y * v.y
