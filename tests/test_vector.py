a2f943912767b064bd1a6b62d270c151bbee2be8
solal <solal.grimbert@gmail.com>
2021-03-09 19:44:27 +0100

    correct import

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,8 +1,15 @@
 from vector import Vector
+from math import pi
+
 
 v = Vector(3, 4)
 v2 = Vector(1, 2)
-
+v3 = Vector(8, 0)
+v4 = Vector(0, 8)
+v5 = Vector(8, 8)
+v6 = Vector(-8, 8)
+v7 = Vector(-8, -8)
+v8 = Vector(8, -8)
 
 def test_norm():
     assert v.norm == 5
@@ -15,3 +22,15 @@ def test_access():
 
 def test_product():
     assert v.scalar(v2) == 11
+
+
+def test_argument():
+    assert v3.arg == 0
+    assert v4.arg == pi/2
+    assert round(v5.arg, 6) == round(pi/4, 6)
+    assert round(v6.arg, 6) == round(pi/4, 6)
+    assert round(v7.arg, 6) == round(-3*pi/4, 6)
+    assert round(v8.arg, 6) == round(-pi/4, 6)
+
+
+
--- vector/vector.py
+++ vector/vector.py
@@ -1,3 +1,16 @@
+import math
+
+
 class Vector:
     def __init__(self, x, y):
-        pass
+        self.x = x
+        self.y = y
+        self.norm = math.sqrt(x * x + y * y)
+        self.arg = math.asin(y/self.norm)
+        
+    def scalar(self, v):
+        return v.x * self.x + v.y * self.y
+
+
+
+
