81e4ac8bbedab86ebd527e0c9c4bc303b0dc4cb4
solal <solal.grimbert@gmail.com>
2021-03-23 19:36:12 +0100

    correct argument

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,6 +1,6 @@
 from vector import Vector
 from math import pi
-
+from math import sqrt
 
 v = Vector(3, 4)
 v2 = Vector(1, 2)
@@ -10,6 +10,9 @@ v5 = Vector(8, 8)
 v6 = Vector(-8, 8)
 v7 = Vector(-8, -8)
 v8 = Vector(8, -8)
+v9 = Vector(sqrt(3)/2, 1/2)
+v10 = Vector(-sqrt(3)/2, 1/2)
+
 
 def test_norm():
     assert v.norm == 5
@@ -27,10 +30,18 @@ def test_product():
 def test_argument():
     assert v3.arg == 0
     assert v4.arg == pi/2
-    assert round(v5.arg, 6) == round(pi/4, 6)
-    assert round(v6.arg, 6) == round(pi/4, 6)
-    assert round(v7.arg, 6) == round(-3*pi/4, 6)
-    assert round(v8.arg, 6) == round(-pi/4, 6)
+    assert round(v5.arg, 6)/pi == round(pi/4, 6)/pi
+    assert round(v6.arg, 6)/pi == round(3*pi/4, 6)/pi
+    assert round(v7.arg, 6)/pi == round(-3*pi/4, 6)/pi
+    assert round(v8.arg, 6)/pi == round(-pi/4, 6)/pi
+    assert round(v9.arg, 6)/pi == round(pi/6, 6)/pi
+    assert round(v10.arg, 6)/pi == round(5*pi/6, 6)/pi
+
+
+
+
+
+
 
 
 
--- vector/vector.py
+++ vector/vector.py
@@ -6,8 +6,11 @@ class Vector:
         self.x = x
         self.y = y
         self.norm = math.sqrt(x * x + y * y)
-        self.arg = math.asin(y/self.norm)
-        
+        if self.y > 0:
+            self.arg = math.acos(x/self.norm)
+        else:
+            self.arg = -math.acos(x/self.norm)
+
     def scalar(self, v):
         return v.x * self.x + v.y * self.y
 
