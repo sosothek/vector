d41818342faaeac7360d51342d79fc914074033a
pcotte <pcotte@advestis.com>
2021-03-23 19:36:07 +0100

    new: usr: implementing overloads !wip

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,5 +1,6 @@
 from vector import Vector
 from math import pi
+import pytest
 
 v10 = Vector(0, 0)
 v3 = Vector(1, 0)
@@ -21,9 +22,9 @@ def test_norm():
 def test_argument():
     assert v10.arg is None
     assert v3.arg == 0
-    assert v4.arg == pi / 2.
+    assert v4.arg == pi / 2.0
     assert v5.arg == pi
-    assert v11.arg == - pi / 2.
+    assert v11.arg == -pi / 2.0
     assert round(v6.arg, 6) == round(pi / 4, 6)
     assert round(v8.arg, 6) == round(3 * pi / 4, 6)
     assert round(v9.arg, 6) == -round(3 * pi / 4, 6)
@@ -37,3 +38,52 @@ def test_access():
 
 def test_product():
     assert v.scalar(v2) == 11
+
+
+@pytest.mark.parametrize("value, expected", [(v2, TypeError()), (3, Vector(9, 12))])
+def test_mul(value, expected):
+    if isinstance(expected, Exception):
+        with pytest.raises(type(expected)):
+            _ = v * value
+    else:
+        assert v * value == expected
+
+
+def test_add_and_equal():
+    assert v + v2 == Vector(4, 6)
+
+
+def test_sub():
+    assert v - v2 == Vector(2, 2)
+
+
+@pytest.mark.parametrize(
+    "theta, expected", [(0, v), (pi, Vector(-v.x, -v.y)), (2 * pi, v), (pi / 2.0, Vector(-v.y, v.x))]
+)
+def test_rotate(theta, expected):
+    rot = v.rotate(theta)
+    print(theta, v, expected, rot)
+    assert rot == expected
+
+
+def test_neg():
+    assert -v == Vector(-3, -4)
+
+
+def test_pos():
+    assert +v == v
+
+
+@pytest.mark.parametrize("value, expected", [(v2, TypeError()), (3, Vector(9, 12))])
+def test_mul(value, expected):
+    if isinstance(expected, Exception):
+        with pytest.raises(type(expected)):
+            _ = value * v
+    else:
+        assert value * v == expected
+
+
+def test_iadd():
+    a = Vector(v)
+    a += v2
+    assert a == Vector(4, 6)
--- vector/vector.py
+++ vector/vector.py
@@ -1,21 +1,34 @@
 import math
+from typing import Union
 
 
 class Vector:
-    def __init__(self, x, y):
+
+    PRECISION = 6
+
+    # noinspection PyTypeChecker
+    def __init__(self, x: Union[int, float, "Vector"], y: Union[int, float] = None):
+        if y is None and not isinstance(x, Vector):
+            raise ValueError("Please specify y or give a Vector as first argument")
+        if y is None:
+            self.x = x.x
+            self.y = x.y
+            self.r = x.norm
+            self.arg = x.arg
+            return
         self.x = x
         self.y = y
-        self.norm = math.sqrt(x*x + y*y)
+        self.norm = math.sqrt(x * x + y * y)
         if x == 0 and y == 0:
             self.arg = None
         elif x > 0 and y == 0:
             self.arg = 0
         elif x == 0 and y > 0:
-            self.arg = math.pi / 2.
+            self.arg = math.pi / 2.0
         elif x < 0 and y == 0:
             self.arg = math.pi
         elif x == 0 and y < 0:
-            self.arg = -math.pi / 2.
+            self.arg = -math.pi / 2.0
         elif y > 0:
             self.arg = math.acos(x / self.norm)
         else:
@@ -23,3 +36,60 @@ class Vector:
 
     def scalar(self, v):
         return self.x * v.x + self.y * v.y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __iadd__(self, other):
+        v = self + other
+        self.x = v.x
+        self.y = v.y
+        self.norm = v.norm
+        self.arg = v.arg
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __isub__(self, other):
+        v = self - other
+        self.x = v.x
+        self.y = v.y
+        self.norm = v.norm
+        self.arg = v.arg
+
+    def __neg__(self):
+        return Vector(-self.x, -self.y)
+
+    def __pos__(self):
+        return Vector(self.x, self.y)
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        else:
+            raise TypeError("Can only multiply a vector by an number")
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __imul__(self, other):
+        v = self * other
+        self.x = v.x
+        self.y = v.y
+        self.r = v.r
+        self.arg = v.arg
+
+    def __eq__(self, other):
+        # noinspection PyTypeChecker
+        return round(self.x, Vector.PRECISION) == round(other.x, Vector.PRECISION) and round(
+            self.y, Vector.PRECISION
+        ) == round(other.y, Vector.PRECISION)
+
+    def __str__(self):
+        return f"Vector({self.x}, {self.y})"
+
+    def rotate(self, theta):
+        # noinspection PyTypeChecker
+        return Vector(
+            self.x * math.cos(theta) - self.y * math.sin(theta), self.y * math.cos(theta) + self.x * math.sin(theta)
+        )
