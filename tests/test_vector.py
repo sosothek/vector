1165c34a30c4501fea7dc0deebf6193d6f7e354c
pcotte <pcotte@advestis.com>
2021-03-23 18:25:52 +0100

    chg: usr: better argument

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,16 +1,17 @@
 from vector import Vector
-from math import pi, sqrt
+from math import pi
 
-v = Vector(3, 4)
-v2 = Vector(1, 2)
+v10 = Vector(0, 0)
 v3 = Vector(1, 0)
 v4 = Vector(0, 1)
 v5 = Vector(-1, 0)
+v11 = Vector(0, -1)
 v6 = Vector(1, 1)
-v7 = Vector(1, -1)
 v8 = Vector(-1, 1)
 v9 = Vector(-1, -1)
-v10 = Vector(0, 0)
+v7 = Vector(1, -1)
+v2 = Vector(1, 2)
+v = Vector(3, 4)
 
 
 def test_norm():
@@ -18,14 +19,15 @@ def test_norm():
 
 
 def test_argument():
+    assert v10.arg is None
     assert v3.arg == 0
     assert v4.arg == pi / 2.
     assert v5.arg == pi
+    assert v11.arg == - pi / 2.
     assert round(v6.arg, 6) == round(pi / 4, 6)
-    assert round(v7.arg, 6) == -round(pi / 4, 6)
     assert round(v8.arg, 6) == round(3 * pi / 4, 6)
     assert round(v9.arg, 6) == -round(3 * pi / 4, 6)
-    assert v10 is None
+    assert round(v7.arg, 6) == -round(pi / 4, 6)
 
 
 def test_access():
--- vector/vector.py
+++ vector/vector.py
@@ -6,14 +6,20 @@ class Vector:
         self.x = x
         self.y = y
         self.norm = math.sqrt(x*x + y*y)
-        if x != 0 and y >= 0:
-            self.arg = math.acos(x / self.norm)
-        elif y != 0 and x > 0:
-            self.arg = math.asin(y / self.norm)
-        elif x < 0 and y < 0:
-            self.arg = -math.acos(x / self.norm)
-        else:
+        if x == 0 and y == 0:
             self.arg = None
+        elif x > 0 and y == 0:
+            self.arg = 0
+        elif x == 0 and y > 0:
+            self.arg = math.pi / 2.
+        elif x < 0 and y == 0:
+            self.arg = math.pi
+        elif x == 0 and y < 0:
+            self.arg = -math.pi / 2.
+        elif y > 0:
+            self.arg = math.acos(x / self.norm)
+        else:
+            self.arg = -math.acos(x / self.norm)
 
     def scalar(self, v):
         return self.x * v.x + self.y * v.y
