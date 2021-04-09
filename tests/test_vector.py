73e58aa203a58dac665fefac6656612e384ab27c
solal <solal.grimbert@gmail.com>
2021-04-09 18:59:27 +0200

    surchager des opérateurs et mmethode

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,6 +1,7 @@
 from vector import Vector
 from math import pi
 from math import sqrt
+from math import exp
 
 v = Vector(3, 4)
 v2 = Vector(1, 2)
@@ -12,6 +13,9 @@ v7 = Vector(-8, -8)
 v8 = Vector(8, -8)
 v9 = Vector(sqrt(3)/2, 1/2)
 v10 = Vector(-sqrt(3)/2, 1/2)
+v11 = v8 + v2
+v12 = v3 - v4
+
 
 
 def test_norm():
@@ -36,6 +40,40 @@ def test_argument():
     assert round(v8.arg, 6)/pi == round(-pi/4, 6)/pi
     assert round(v9.arg, 6)/pi == round(pi/6, 6)/pi
     assert round(v10.arg, 6)/pi == round(5*pi/6, 6)/pi
+
+
+def test_additionvector():
+    v11 = v8 + v2
+    assert v11 == Vector(9, -6)
+
+
+def test_soustractionvector() :
+    v12 = v3 - v4
+    assert v12 == Vector(8, -8)
+
+
+def test_multiplicationvector():
+    v13 = v2*3
+    assert v13 == Vector(3, 6)
+
+
+def test_rmultiplicationvector():
+    v13 = 3*v2
+    assert v13 == Vector(3, 6)
+
+
+def test_rsoustractionvector() :
+    v14 = v4 - v3
+    assert v14 == Vector(-8, 8)
+
+
+def test_exp():
+    exp(v)
+
+
+
+
+
 
 
 
--- vector/vector.py
+++ vector/vector.py
@@ -14,6 +14,52 @@ class Vector:
     def scalar(self, v):
         return v.x * self.x + v.y * self.y
 
+    def __add__(self, v):
+        x = v.x + self.x
+        y = v.y + self.y
+        return Vector(x, y)
+
+    def __eq__(self, v):
+        a = self.x == v.x
+        i = self.y == v.y
+        return a and i
+
+    def __sub__(self, v):
+        s = self.x - v.x
+        j = self.y - v.y
+        return Vector(s, j)
+
+    def __mul__(self, a):
+        h = a*self.x
+        k = a*self.y
+        return Vector(h, k)
+
+    def __rmul__(self, a):
+        return self*a
+
+    def __rsub__(self, v):
+        s = -self.x + v.x
+        j = -self.y + v.y
+        return Vector(s, j)
+
+    def __truediv__(self, a):
+        return self*(1/a)
+
+    def exp(self):
+        return Vector(1, 1)*math.cosh(self.norm) + (self/self.norm)*math.sinh(self.norm)
+
+
+built_exp = math.exp
+
+
+def newexp(x):
+    if isinstance(x, Vector):
+        return x.exp()
+    else:
+        return built_exp(x)
+
+
+math.exp = newexp
 
 
 
